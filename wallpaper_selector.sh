c261d44881de234c8fdd746705bc700806e0c131
HORIZON-Donut <kiettisak.k2546@gmail.com>
2026-03-08 15:36:25 +0700

    Remove unfinished feature out from script

--- wallpaper_selector.sh
+++ wallpaper_selector.sh
@@ -11,6 +11,4 @@ index_value=$(( (current_time % number_of_wallpaper) - 1))
 echo "Set random wallpaper to ${wallpaper_list[$index_value]}"
 ln -sf ${wallpaper_list[$index_value]} ~/pic/wallpaper/main_desktop_wallpaper
 
-walldir=$(1:$HOME/pic/wallpaper/desktop_wallpaper)
-PREVIEW=true rofi -no-config -theme fullscreen-preview.rasi -show filebrowser -filebrowser-command 'setbg' -filebrowser-directory "$walldir" -filebrowser-sorting-method mtime -selected-row 1 >/dev/null
-#feh --bg-fill /home/horizon/pic/wallpaper/main_desktop_wallpaper
+feh --bg-fill /home/horizon/pic/wallpaper/main_desktop_wallpaper
