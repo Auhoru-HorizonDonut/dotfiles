d723bda1d565cb1b40fac69c353fead098387657
HORIZON-Donut <kiettisak.k2546@gmail.com>
2026-03-08 15:32:31 +0700

    Include command rofi that allow it to show previews

--- wallpaper_selector.sh
+++ wallpaper_selector.sh
@@ -8,7 +8,9 @@ number_of_wallpaper=${#wallpaper_list[@]}
 current_time=$(date +%s%3N)
 index_value=$(( (current_time % number_of_wallpaper) - 1))
 
-echo "Set wallpaper to ${wallpaper_list[$index_value]}"
+echo "Set random wallpaper to ${wallpaper_list[$index_value]}"
 ln -sf ${wallpaper_list[$index_value]} ~/pic/wallpaper/main_desktop_wallpaper
 
-feh --bg-fill /home/horizon/pic/wallpaper/main_desktop_wallpaper
+walldir=$(1:$HOME/pic/wallpaper/desktop_wallpaper)
+PREVIEW=true rofi -no-config -theme fullscreen-preview.rasi -show filebrowser -filebrowser-command 'setbg' -filebrowser-directory "$walldir" -filebrowser-sorting-method mtime -selected-row 1 >/dev/null
+#feh --bg-fill /home/horizon/pic/wallpaper/main_desktop_wallpaper
