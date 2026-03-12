9b6c2713d45f7e94362a536ccc5f5ab7ce3c4c9d
HORIZON-Donut <kiettisak.k2546@gmail.com>
2026-03-12 19:38:04 +0700

    Reduce complexity in wallpaper selector script by make it immedietly use picked image instead of create symlink

--- wallpaper_selector.sh
+++ wallpaper_selector.sh
@@ -2,13 +2,12 @@
 
 shopt -s nullglob
 
-wallpaper_list=(~/pic/wallpaper/desktop_wallpaper/*)
+wallpaper_list=(~/pic/wallpaper/*)
 number_of_wallpaper=${#wallpaper_list[@]}
 
 current_time=$(date +%s%3N)
 index_value=$(( (current_time % number_of_wallpaper) - 1))
 
 echo "Set random wallpaper to ${wallpaper_list[$index_value]}"
-ln -sf ${wallpaper_list[$index_value]} ~/pic/wallpaper/main_desktop_wallpaper
 
-feh --bg-fill /home/horizon/pic/wallpaper/main_desktop_wallpaper
+feh --bg-fill ${wallpaper_list[$index_value]}
