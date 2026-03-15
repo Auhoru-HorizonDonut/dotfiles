30f67b8a4d134ea2ce1f3257f1188296ca644180
HORIZON-Donut <kiettisak.k2546@gmail.com>
2026-03-15 23:23:09 +0700

    Made wallpaper selection script corespond to lock screen wallpaper

--- wallpaper_selector.sh
+++ wallpaper_selector.sh
@@ -2,7 +2,20 @@
 
 shopt -s nullglob
 
-wallpaper_list=($HOME/.wallpaper/*)
+case $@ in
+	0)
+		command="feh --bg-fill" 
+		wallpaper_list=($HOME/.wallpaper/*)
+		;;
+	1)
+		command="i3lock -f -k -e -p win -i"
+		wallpaper_list=($HOME/.wallpaper/*png)
+		;;
+	*)
+		command="feh --bg-fill"
+		wallpaper_list=($HOME/.wallpaper/*)
+		;;
+esac
 number_of_wallpaper=${#wallpaper_list[@]}
 
 current_time=$(date +%s%3N)
@@ -10,4 +23,4 @@ index_value=$(( (current_time % number_of_wallpaper) - 1))
 
 echo "Set random wallpaper to ${wallpaper_list[$index_value]}"
 
-feh --bg-fill ${wallpaper_list[$index_value]}
+$command ${wallpaper_list[$index_value]}
